c5616f83bf846f39f064dd612d7d5be942ed8acc
David Carpenter <carpenterd777@gmail.com>
2022-02-23 22:41:27 -0500

    added docstrings to each place type

--- placeTypes.ts
+++ placeTypes.ts
@@ -1,57 +1,69 @@
 import type { ObjectId } from "mongodb";
 import type { Place as GooglePlace } from "@googlemaps/google-maps-services-js";
 
-// Place type containing the place data pulled from the google api along with accesibility data
+/**
+ * Contains place data from the Google Places API along with accesibility
+ * and promotion data from the Peer database.
+ */
 export type PlaceWithAccesibilityData = GooglePlace & {
-	accessibilityData?: {
-		_id: string;
-		guideDogAvg: number;
-		isMenuAccessibleAvg: number;
-		noiseLevelAvg: number;
-		lightingAvg: number;
-		isStaffHelpfulAvg: number;
-		isBathroomOnEntranceFloorAvg: number;
-		isContactlessPaymentOfferedAvg: number;
-		isStairsRequiredAvg: number;
-		spacingAvg: number;
-		promotion: {
-			monthly_budget: number;
-			max_cpc: number;
-		};
-	};
+  accessibilityData?: {
+    _id: string;
+    guideDogAvg: number;
+    isMenuAccessibleAvg: number;
+    noiseLevelAvg: number;
+    lightingAvg: number;
+    isStaffHelpfulAvg: number;
+    isBathroomOnEntranceFloorAvg: number;
+    isContactlessPaymentOfferedAvg: number;
+    isStairsRequiredAvg: number;
+    spacingAvg: number;
+    promotion: {
+      monthly_budget: number;
+      max_cpc: number;
+    };
+  };
 };
 
-//Place type only containing the place ID along with the accesibility data
-export interface Place {
-	_id: GooglePlace["place_id"];
-	guideDogAvg: number | null;
-	isMenuAccessibleAvg: number | null;
-	noiseLevelAvg: number | null;
-	lightingAvg: number | null;
-	isStaffHelpfulAvg: number | null;
-	isBathroomOnEntranceFloorAvg: number | null;
-	isContactlessPaymentOfferedAvg: number | null;
-	isStairsRequiredAvg: number | null;
-	spacingAvg: number | null;
-	promotion: {
-		monthly_budget: number;
-		max_cpc: number;
-	};
-}
-
-// Place type containing the place data pulled from the google api, the custom Place type, and promotion types
-export type PlaceWithA11yAndPromo = GooglePlace & Place & {
-	promoMonth?: PromotionMonth;
-	isValidPromo?: boolean;
-	isPromoted?: boolean;
-	spend_amount?: number;
+/**
+ * Contains only accessibility and promotion data from the Peer database.
+ * This is the type of the Place documents from the database.
+ */
+export type Place = {
+  _id: GooglePlace["place_id"];
+  guideDogAvg: number | null;
+  isMenuAccessibleAvg: number | null;
+  noiseLevelAvg: number | null;
+  lightingAvg: number | null;
+  isStaffHelpfulAvg: number | null;
+  isBathroomOnEntranceFloorAvg: number | null;
+  isContactlessPaymentOfferedAvg: number | null;
+  isStairsRequiredAvg: number | null;
+  spacingAvg: number | null;
+  promotion: {
+    monthly_budget: number;
+    max_cpc: number;
+  };
 };
 
-// Promotion month type to be used within the PlaceWithA11yAndPromo type
-export interface PromotionMonth {
-	_id?: ObjectId;
-	placeID: GooglePlace["place_id"];
-	month: number;
-	year: number;
-	totalSpent: number;
-}
+/**
+ * Contains place data from the Google Places API, the Peer database on accessibility,
+ * and more detailed information about the promotion status of this location.
+ */
+export type PlaceWithA11yAndPromo = GooglePlace &
+  Place & {
+    promoMonth?: PromotionMonth;
+    isValidPromo?: boolean;
+    isPromoted?: boolean;
+    spend_amount?: number;
+  };
+
+/**
+ * Tracks information about when a location paid for a promotion with Peer.
+ */
+export type PromotionMonth = {
+  _id?: ObjectId;
+  placeID: GooglePlace["place_id"];
+  month: number;
+  year: number;
+  totalSpent: number;
+};
