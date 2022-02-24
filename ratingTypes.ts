369e0f759ccc103d710adcdd35c186853002d387
David Carpenter <carpenterd777@gmail.com>
2022-02-23 22:46:08 -0500

    add prettier

--- ratingTypes.ts
+++ ratingTypes.ts
@@ -9,44 +9,44 @@ type YesNoRating = 0 | 1 | null;
  * A single rating from a user on a place.
  */
 export interface Rating {
-  _id?: ObjectId;
-  userID: ObjectId;
-  placeID: GooglePlace["place_id"];
-  guideDogFriendly: number | null;
-  isMenuAccessible: YesNoRating;
-  noiseLevel: number | null;
-  lighting: number | null;
-  isStaffHelpful: YesNoRating;
-  isBathroomOnEntranceFloor: YesNoRating;
-  isContactlessPaymentOffered: YesNoRating;
-  isStairsRequired: YesNoRating;
-  spacing: number | null;
-  comment: string | null;
-  dateCreated: Date;
-  dateEdited?: Date;
+	_id?: ObjectId;
+	userID: ObjectId;
+	placeID: GooglePlace["place_id"];
+	guideDogFriendly: number | null;
+	isMenuAccessible: YesNoRating;
+	noiseLevel: number | null;
+	lighting: number | null;
+	isStaffHelpful: YesNoRating;
+	isBathroomOnEntranceFloor: YesNoRating;
+	isContactlessPaymentOffered: YesNoRating;
+	isStairsRequired: YesNoRating;
+	spacing: number | null;
+	comment: string | null;
+	dateCreated: Date;
+	dateEdited?: Date;
 }
 
 /**
  * Maps name of an average rating field to a readable string.
  */
 export const fieldsToNamesMap: Record<string, string> = {
-  avgGuideDogFriendly: "Guide Dog Friendliness",
-  isMenuAccessibleAvg: "Menu Accessibility",
-  noiseLevelAvg: "Noise Level",
-  lightingAvg: "Lighting",
-  isStaffHelpfulAvg: "Staff Helpfulness",
-  isBathroomOnEntranceFloorAvg: "Bathroom On Entrance Floor",
-  isContactlessPaymentOfferedAvg: "Contactless Payment Offered",
-  isStairsRequiredAvg: "Stairs Required",
-  spacingAvg: "Spacing",
+	avgGuideDogFriendly: "Guide Dog Friendliness",
+	isMenuAccessibleAvg: "Menu Accessibility",
+	noiseLevelAvg: "Noise Level",
+	lightingAvg: "Lighting",
+	isStaffHelpfulAvg: "Staff Helpfulness",
+	isBathroomOnEntranceFloorAvg: "Bathroom On Entrance Floor",
+	isContactlessPaymentOfferedAvg: "Contactless Payment Offered",
+	isStairsRequiredAvg: "Stairs Required",
+	spacingAvg: "Spacing",
 };
 
 function buildNamesToFieldsMap() {
-  let namesToFieldsMap: Record<string, string> = {};
-  for (let key in fieldsToNamesMap) {
-    namesToFieldsMap[fieldsToNamesMap[key]] = key;
-  }
-  return namesToFieldsMap;
+	let namesToFieldsMap: Record<string, string> = {};
+	for (let key in fieldsToNamesMap) {
+		namesToFieldsMap[fieldsToNamesMap[key]] = key;
+	}
+	return namesToFieldsMap;
 }
 
 /*
